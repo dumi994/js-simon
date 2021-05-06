2364122cfc59586c1db4094fb04c494d6614ef0c
dumi994 <dumi.9944@gmail.com>
2021-05-06 17:57:35 +0200

    generazione casuale

--- main.js
+++ main.js
@@ -1,36 +1,20 @@
 // Un alert() espone 5 numeri generati casualmente.
 var arrayPc = [];
 var arrayUtente = [];
-var numMax = 100;
-
 function randomNumb(min, max){
-   Math.floor(Math.random()*(max - min + 1) + min);
+   return Math.floor(Math.random()* (max - min + 1) + min);
 }
-
-
-while (arrayPc.length < 5){
-    number = randomNumb (1, numMax);
-    if(!arrayPc.includes(number)){
-        arrayPc.push(number);
-    }
-} 
+console.log(randomNumb(1, 100));
+for (var i = 0; i < 5; i++){
+    arrayPc.push(randomNumb(1, 100))
+}
 console.log(arrayPc);
 
+alert (`Memorizza questi numeri, ${arrayPc}`);
 
 
-/* function randomNumber (){
-    var pcNumb = Math.floor(Math.random() * (100 - 1))+ 1;
-    console.log(pcNumb); 
-    arrayPc.push(pcNumb);
-    for (var i =0; i < 5; i++){
-        arrayPc = pcNumb[i];
 
-        if (array.length < 5){
-        pcNumb;
-        };
-    }
-    
-} */
+
 
 // Da li parte un timer di 30 secondi.
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
